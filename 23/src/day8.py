0eb00309c434d0b9e9ceec41ef7ad1ae686ceb1f
JustWannaFly <joshua.korver@datasite.com>
2023-12-11 17:18:59 -0600

    Day 8 part 2

--- 23/src/day8.py
+++ 23/src/day8.py
@@ -9,6 +9,18 @@ testInput = [
 'BBB = (AAA, ZZZ)',
 'ZZZ = (ZZZ, ZZZ)'
 ]
+part2Test = [
+'LR',
+'',
+'11A = (11B, XXX)',
+'11B = (XXX, 11Z)',
+'11Z = (11B, XXX)',
+'22A = (22B, XXX)',
+'22B = (22C, 22C)',
+'22C = (22Z, 22Z)',
+'22Z = (22B, 22B)',
+'XXX = (XXX, XXX)'
+]
 
 def parseInput(data):
     desertMap = {}
@@ -38,7 +50,35 @@ def part1(data):
 
     print('steps taken: ' + str(steps))
 
-def part2(data):
-    print('I dunno yet')
+def allEndInZ(locations):
+    for loc in locations:
+        if not loc[len(loc)-1] == 'Z':
+            return False
+    return True
 
-part1(input)
+def getStartingLocations(desertMap):
+    locations = []
+    for key in desertMap.keys():
+        if key[len(key)-1] == 'A':
+            locations.append(key)
+    return locations
+
+
+def part2(data):
+    directions, desertMap = parseInput(data)
+    locations = getStartingLocations(desertMap)
+    steps = 0
+    while not allEndInZ(locations):
+        direction = directions[steps % len(directions)]
+        steps = steps + 1
+        targetIndex = 0
+        if direction == 'R':
+            targetIndex = 1
+        newLocations = []
+        for location in locations:
+            newLocations.append(desertMap[location][targetIndex])
+        locations = newLocations
+
+    print('steps taken: ' + str(steps))
+
+part2(input)
